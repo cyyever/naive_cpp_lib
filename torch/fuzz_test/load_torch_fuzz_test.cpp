62c84f1eb070bbbd0d60b0ba998d28521aceab45
cyy <cyyever@outlook.com>
2020-04-21 23:37:42 +0800

    fix test

--- torch/fuzz_test/load_torch_fuzz_test.cpp
+++ torch/fuzz_test/load_torch_fuzz_test.cpp
@@ -7,16 +7,19 @@
 #include <torch/csrc/api/include/torch/serialize.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  std::istringstream is;
-  if (Size == 0) {
-    is = std::istringstream("");
-  } else {
-    is = std::istringstream(
-        std::string(reinterpret_cast<const char *>(Data), Size));
+  try {
+    std::istringstream is;
+    if (Size == 0) {
+      is = std::istringstream("");
+    } else {
+      is = std::istringstream(
+          std::string(reinterpret_cast<const char *>(Data), Size));
+    }
+
+    torch::Tensor value;
+    torch::load(value, is);
+
+  } catch (...) {
   }
-
-  torch::Tensor value;
-  torch::load(value, is);
-
   return 0; // Non-zero return values are reserved for future use.
 }
