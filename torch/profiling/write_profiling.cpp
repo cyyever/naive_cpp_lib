af0ee4b0b4c07fa769d14638588e68f7399091b4
cyyever <cyyever@outlook.com>
2020-07-24 21:44:32 +0800

    fix code

--- torch/profiling/write_profiling.cpp
+++ torch/profiling/write_profiling.cpp
@@ -6,18 +6,33 @@
  */
 
 #include <chrono>
+#include <filesystem>
 #include <torch/torch.h>
 
-#include "log/log.hpp"
-#include "util/time.hpp"
+ uint64_t now_ms() {
+    return static_cast<uint64_t>(
+        std::chrono::time_point_cast<std::chrono::milliseconds>(
+            std::chrono::steady_clock::now())
+            .time_since_epoch()
+            .count());
+  }
+
 int main(int argc, char **argv) {
 
-  auto begin_ms = cyy::cxx_lib::time::now_ms<std::chrono::steady_clock>();
   auto tensor = torch::randn({1, 200 * 1024});
+  auto begin_ms =now_ms();
   for (int i = 0; i < 100; i++) {
-    torch::save(value, "tmp_file");
+    torch::save(tensor, "tmp_file");
   }
-  auto end_ms = cyy::cxx_lib::time::now_ms<std::chrono::steady_clock>();
+  auto end_ms = now_ms();
+  std::cout << "insertion used " << end_ms - begin_ms << " ms" << std::endl;
+
+  begin_ms = now_ms();
+  for (int i = 0; i < 100; i++) {
+    std::filesystem::remove("tmp_file");
+    torch::save(tensor, "tmp_file");
+  }
+  end_ms = now_ms();
   std::cout << "insertion used " << end_ms - begin_ms << " ms" << std::endl;
 
   return 0;
