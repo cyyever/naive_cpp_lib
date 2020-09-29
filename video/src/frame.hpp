927a705e58d647cf7294a16bb290ae982ef00a2f
cyy <cyyever@outlook.com>
2020-09-29 17:47:36 +0700

    fix code

--- video/src/frame.hpp
+++ video/src/frame.hpp
@@ -11,16 +11,16 @@
 #include <opencv2/opencv.hpp>
 
 namespace deepir {
-namespace video {
-
-//! \brief 视频帧
-//! \note 为了提高性能，我们希望尽量模拟ffmpeg等的帧结构，避免无谓的复制和转换
-struct frame final {
-  uint64_t seq{};     //!< 帧序号
-  cv::Mat content;    //!< 帧内容
-  bool is_key{false}; //!< 标志是否关键帧
-};
-} // namespace video
+  namespace video {
+    //! \brief 视频帧
+    //! \note
+    //! 为了提高性能，我们希望尽量模拟ffmpeg等的帧结构，避免无谓的复制和转换
+    struct frame final {
+      uint64_t seq{};     //!< 帧序号
+      cv::Mat content;    //!< 帧内容
+      bool is_key{false}; //!< 标志是否关键帧
+    };
+  } // namespace video
 } // namespace deepir
 
 #endif
