601c96bd5e97fa79245e2f3561a95d4fe763e742
cyy <cyyever@outlook.com>
2021-02-08 20:52:32 +0700

    fix code

---
+++ torch/python_binding/synced_tensor_dict.cpp
@@ -0,0 +1,50 @@
+#include <pybind11/pybind11.h>
+#include <torch/extension.h>
+#include "../synced_sparse_tensor_dict.hpp"
+#include "../synced_tensor_dict.hpp"
+namespace py = pybind11;
+PYBIND11_MODULE(cyy_naive_cpp_extension , m) {
+  using synced_tensor_dict = cyy::naive_lib::pytorch::synced_tensor_dict;
+  using synced_sparse_tensor_dict =
+      cyy::naive_lib::pytorch::synced_sparse_tensor_dict;
+  auto sub_m = m.def_submodule("data_structure", "Contains data structures");
+  py::class_<synced_tensor_dict>(sub_m, "SyncedTensorDict")
+      .def(py::init<const std::string &>())
+      .def("prefetch", (void (synced_tensor_dict::*)(
+                           const std::vector<std::string> &keys)) &
+                           synced_tensor_dict::prefetch)
+      .def("set_in_memory_number", &synced_tensor_dict::set_in_memory_number)
+      .def("get_in_memory_number", &synced_tensor_dict::get_in_memory_number)
+      .def("set_storage_dir", &synced_tensor_dict::set_storage_dir)
+      .def("get_storage_dir", &synced_tensor_dict::get_storage_dir)
+      .def("set_permanent_storage",
+           &synced_tensor_dict::enable_permanent_storage)
+      .def("enable_permanent_storage",
+           &synced_tensor_dict::enable_permanent_storage)
+      .def("disable_permanent_storage",
+           &synced_tensor_dict::disable_permanent_storage)
+      .def("set_wait_flush_ratio", &synced_tensor_dict::set_wait_flush_ratio)
+      .def("set_saving_thread_number",
+           &synced_tensor_dict::set_saving_thread_number)
+      .def("set_fetch_thread_number",
+           &synced_tensor_dict::set_fetch_thread_number)
+      .def("set_logging", &synced_tensor_dict::set_logging)
+      .def("__setitem__", &synced_tensor_dict::emplace)
+      .def("__len__", &synced_tensor_dict::size)
+      .def("__contains__", &synced_tensor_dict::contains)
+      .def("__getitem__", &synced_tensor_dict::get)
+      .def("__delitem__", &synced_tensor_dict::erase)
+      .def("keys", &synced_tensor_dict::keys)
+      .def("in_memory_keys", &synced_tensor_dict::in_memory_keys)
+      .def("release", &synced_tensor_dict::release)
+      .def("clear", &synced_tensor_dict::clear)
+      .def("flush_all", &synced_tensor_dict::flush_all,
+           "flush all in-memory data to the disk", py::arg("wait") = true)
+      .def("flush",
+           (void (synced_tensor_dict::*)(size_t)) & synced_tensor_dict::flush);
+  py::class_<synced_sparse_tensor_dict, synced_tensor_dict>(
+      sub_m, "SyncedSparseTensorDict")
+      .def(py::init<torch::Tensor, torch::IntArrayRef, const std::string &>())
+      .def("__getitem__", &synced_sparse_tensor_dict::get)
+      .def("__setitem__", &synced_sparse_tensor_dict::emplace);
+}
