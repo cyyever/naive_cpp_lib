df9339f77959a77d12f904563d4929bb2f4d8a24
cyy <cyyever@outlook.com>
2021-02-09 18:42:17 +0700

    fix pybind

--- python_binding/synced_tensor_dict.hpp
+++ python_binding/synced_tensor_dict.hpp
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <torch/extension.h>
-#include "src/torch/synced_sparse_tensor_dict.hpp"
-#include "src/torch/synced_tensor_dict.hpp"
+#include "torch/synced_sparse_tensor_dict.hpp"
+#include "torch/synced_tensor_dict.hpp"
 namespace py = pybind11;
 inline void define_torch_extension(  py::module_ &m) {
   using synced_tensor_dict = cyy::naive_lib::pytorch::synced_tensor_dict;
