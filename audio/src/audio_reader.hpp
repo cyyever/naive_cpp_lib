dd7baf0874344c7e4afba68bc2793f7bebaf1b4f
cyy <cyyever@outlook.com>
2021-02-23 12:23:17 +0700

    refactor code

---
+++ audio/src/audio_reader.hpp
@@ -0,0 +1,39 @@
+/*!
+ * \file audio_reader.hpp
+ *
+ * \brief sound频讀取接口类
+ * \author yuewu,cyy
+ */
+
+#pragma once
+
+#include <chrono>
+#include <optional>
+#include <string>
+
+namespace cyy::naive_lib::audio {
+
+  //! brief 视频讀取接口类
+  class reader {
+  public:
+    reader() = default;
+
+    reader(const reader &) = delete;
+    reader &operator=(const reader &) = delete;
+
+    reader(reader &&) = default;
+    reader &operator=(reader &&) = default;
+
+    virtual ~reader() = default;
+
+    //! \brief 打開视频
+    //! \param url 视频地址
+    //! \note 先关闭之前打开的视频再打开此视频
+    virtual bool open(const std::string &url) = 0;
+
+    //! \brief 關閉视频
+    virtual void close() = 0;
+
+    virtual std::optional<std::chrono::milliseconds> get_duration() = 0;
+  };
+} // namespace cyy::naive_lib::audio
---
+++ audio/src/ffmpeg_audio_reader.hpp
@@ -0,0 +1,35 @@
+/*!
+ * \file ffmpeg_audio_reader.h
+ *
+ * \brief 封装ffmpeg对sound频流的讀操作
+ * \author Yue Wu,cyy
+ */
+#pragma once
+
+#include <memory>
+
+#include "audio_reader.hpp"
+namespace cyy::naive_lib::audio::ffmpeg {
+
+  class reader_impl;
+  //! \brief 封装ffmpeg对视频流的讀操作
+  class reader final : public ::cyy::naive_lib::audio::reader {
+  public:
+    reader();
+
+    ~reader() override;
+
+    //! \brief 打开视频
+    //! \param url 视频地址，如果是本地文件，使用file://协议
+    //! \note 先关闭之前打开的视频再打开此url对应的视频
+    bool open(const std::string &url) override;
+
+    //! \brief 关闭已经打开的视频，如果之前没调用过open，调用该函数无效果
+    void close() override;
+
+    std::optional<std::chrono::milliseconds> get_duration() override;
+
+  private:
+    std::unique_ptr<reader_impl> pimpl;
+  };
+} // namespace cyy::naive_lib::audio::ffmpeg
