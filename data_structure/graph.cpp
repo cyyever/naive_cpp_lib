8a083238614f7f296703a861223b5bb9cdf90ea5
cyy <cyyever@outlook.com>
2021-10-03 22:44:35 +0800

    fix code

---
+++ data_structure/graph.cpp
@@ -0,0 +1,52 @@
+/*!
+ * \file graph.hpp
+ *
+ * \brief implements graph
+ */
+
+#include "graph.hpp"
+
+namespace cyy::naive_lib::data_structure {
+
+  /* std::optional<size_t> graph::get_node_index(node_type node) const { */
+  /*   auto it = node_indices.find(node); */
+  /*   if (it != node_indices.end()) { */
+  /*     return it->second; */
+  /*   } */
+  /*   return {}; */
+  /* } */
+  std::pair<graph::node_index_map_type, graph::adjacent_matrix_type>
+  graph::get_adjecent_matrix() const {
+    node_index_map_type node_indices;
+    adjacent_matrix_type adjacent_matrix;
+    node_indices.reserve(adjacent_list.size());
+    adjacent_matrix.reserve(adjacent_list.size());
+
+    for (auto const &[node, _] : adjacent_list) {
+      node_indices[node] = adjacent_matrix.size();
+      adjacent_matrix.emplace_back(adjacent_list.size(), false);
+    }
+    for (auto const &[node, adjacent_nodes] : adjacent_list) {
+      auto from_index = node_indices[node];
+      for (auto const &to_node : adjacent_nodes) {
+        auto to_index = node_indices[to_node];
+        adjacent_matrix[from_index][to_index] = true;
+      }
+    }
+    return {std::move(node_indices), std::move(adjacent_matrix)};
+  }
+  void graph::add_edge(const edge_type &edge) {
+    add_directed_edge(edge);
+    auto reversed_edge = edge;
+    std::swap(reversed_edge.first, reversed_edge.second);
+    add_directed_edge(reversed_edge);
+  }
+  void graph::add_directed_edge(const edge_type &edge) {
+    /* auto [it, has_inserted] = */
+    /*     node_indices.try_emplace(edge.first, adjacent_list.size()); */
+    /* if (has_inserted) { */
+    /*   adjacent_list.emplace_back(); */
+    /* } */
+    adjacent_list[edge.first].push_back(edge.second);
+  }
+} // namespace cyy::naive_lib::data_structure
---
+++ data_structure/graph.hpp
@@ -0,0 +1,51 @@
+/*!
+ * \file graph.hpp
+ *
+ * \brief implements graph
+ */
+
+#pragma once
+
+#include <list>
+#include <optional>
+#include <ranges>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace cyy::naive_lib::data_structure {
+
+  class graph {
+  public:
+    using node_type = size_t;
+    using edge_type = std::pair<node_type, node_type>;
+    using node_index_map_type = std::unordered_map<node_type, size_t>;
+    using adjacent_matrix_type = std::vector<std::vector<bool>>;
+    graph() = default;
+    virtual ~graph() = default;
+    template <std::ranges::input_range U>
+    requires std::same_as<edge_type, std::ranges::range_value_t<U>>
+    explicit graph(U edges) {
+      for (auto const &edge : edges) {
+        add_edge(edge);
+      }
+    }
+    /* std::optional<size_t> get_node_index(node_type node) const; */
+    virtual void add_edge(const edge_type &edge);
+    auto const &get_adjecent_list() const { return adjacent_list; }
+    std::pair<node_index_map_type, adjacent_matrix_type>
+    get_adjecent_matrix() const;
+
+  protected:
+    void add_directed_edge(const edge_type &edge);
+
+  private:
+    /* std::unordered_map<node_type, size_t> node_indices; */
+    std::unordered_map<node_type, std::list<node_type>> adjacent_list;
+  };
+  class directed_graph : public graph {
+  public:
+    using graph::graph;
+    void add_edge(const edge_type &edge) override { add_directed_edge(edge); }
+  };
+} // namespace cyy::naive_lib::data_structure
