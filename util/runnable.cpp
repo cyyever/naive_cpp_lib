4731e9e1370d828a79b898d870b187a76aadea20
cyy <cyyever@outlook.com>
2022-08-06 17:24:07 +0800

    fix code

--- util/runnable.cpp
+++ util/runnable.cpp
@@ -11,6 +11,7 @@
 
 namespace cyy::naive_lib {
   void runnable::start(std::string name) {
+    puts("begin start");
     std::lock_guard lock(sync_mutex);
     if (thd.joinable()) {
       throw std::runtime_error("thread is running");
@@ -29,17 +30,19 @@ namespace cyy::naive_lib {
                   std::this_thread::sleep_for(std::chrono::milliseconds(1));
                 }
               }
-#if defined(__linux__)
               if (!name_.empty()) {
                 // glibc 限制名字長度
                 name_.resize(15);
+#if defined(__linux__)
                 auto err = pthread_setname_np(pthread_self(), name_.c_str());
                 if (err != 0) {
                   LOG_ERROR("pthread_setname_np failed:{}",
                             cyy::naive_lib::util::errno_to_str(err));
                 }
-              }
+#elif defined(__FreeBSD__)
+                pthread_setname_np(pthread_self(), name_.c_str());
 #endif
+              }
               run();
             } catch (const std::exception &e) {
               if (exception_callback) {
